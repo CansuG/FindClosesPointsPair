70f059950c0301d89455b5299f405e6d9f9c3f70
CansuG <cansugurel@eskisehir.edu.tr>
2022-04-30 17:21:19 +0300

    In FindClosest.java getClosestPointPair and stripClosest methods are written.

--- src/HW_2/FindClosest.java
+++ src/HW_2/FindClosest.java
@@ -8,41 +8,42 @@ public class FindClosest {
     private PointPair closestPointPair;
     private final QuickSort quicksort = new QuickSort();
 
-    /** Constructor
+    /**
+     * Constructor
      *
      * @param points --> point array
      */
-    public FindClosest(Point2D.Double[] points)
-    {
+    public FindClosest(Point2D.Double[] points) {
         //Sort points by X coordinate
         quicksort.sort(points, 0, points.length - 1, "compareX");
         this.closestPointPair = calculateClosestPointPair(points, 0, points.length - 1);
         //*********************************do nothing***************************************//
     }
 
-    /** Get closest Point Pair
+    /**
+     * Get closest Point Pair
      *
      * @return closestPointPair
      */
-    public PointPair getClosestPointPair()
-    {
+    public PointPair getClosestPointPair() {
         return this.closestPointPair;
     }
 
-    /** Main method for calculate and return closest point pair
+    /**
+     * Main method for calculate and return closest point pair
      *
-     * @param p --> point array
+     * @param p          --> point array
      * @param startIndex --> First index of p[]
-     * @param lastIndex --> last index of p[]
+     * @param lastIndex  --> last index of p[]
      * @return
      */
-    private PointPair calculateClosestPointPair(Point2D.Double[] p, int startIndex, int lastIndex)
-    {
+    private PointPair calculateClosestPointPair(Point2D.Double[] p, int startIndex, int lastIndex) {
         //Write codes here
         return new PointPair(p[startIndex], p[lastIndex]); //This line will deleted
     }
 
-    /** calculate and return closest point pair from 3 points
+    /**
+     * calculate and return closest point pair from 3 points
      *
      * @param p1 --> point 1
      * @param p2 --> point 2
@@ -51,12 +52,25 @@ public class FindClosest {
      */
     private PointPair getClosestPointPair(Point2D.Double p1, Point2D.Double p2, Point2D.Double p3) {
         //Write codes here
-        return new PointPair(p1, p2); //This line will deleted
+
+        PointPair pointPair1_2 = new PointPair(p1, p2);
+        PointPair pointPair2_3 = new PointPair(p2, p3);
+        PointPair pointPair1_3 = new PointPair(p1, p3);
+
+        PointPair pointPairCompared = getClosestPointPair(pointPair1_2, pointPair2_3);
+
+        return getClosestPointPair(pointPairCompared, pointPair1_3);
+
     }
 
-    private PointPair getClosestPointPair(PointPair p1, PointPair p2){
+    private PointPair getClosestPointPair(PointPair p1, PointPair p2) {
         //Write codes here
-        return p1; //This line will deleted
+
+        if (p1.getDistance() < p2.getDistance()) {
+            return p1;
+        } else {
+            return p2;
+        }
     }
 
     /**
@@ -66,14 +80,25 @@ public class FindClosest {
      * Note that this method seems to be a O(n^2) method, but it's a O(n)
      * method as the inner loop runs at most 6 times
      *
-     * @param strip --> point array
-     * @param size --> strip array element count
+     * @param strip        --> point array
+     * @param size         --> strip array element count
      * @param shortestLine --> shortest line calculated so far
      * @return --> new shortest line
      */
     private PointPair stripClosest(Point2D.Double strip[], int size, PointPair shortestLine) {
         //Write codes here
-        return shortestLine; //This line will deleted
+
+        PointPair closestPair = shortestLine;
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                PointPair closestFromStrip = new PointPair(strip[i], strip[j]);
+                if ((strip[j].getY() - strip[i].getY()) < shortestLine.getDistance() && closestFromStrip.getDistance() <= closestPair.getDistance()) {
+                    closestPair = closestFromStrip;
+                }
+            }
+        }
+
+        return closestPair;
     }
 
 }
